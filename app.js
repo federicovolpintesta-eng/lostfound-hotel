ee0943f3f86ac87c013fa015c3e0603f4839a31b
Federico Volpintesta <federicovolpintesta@MacBook-Pro-de-Federico.local>
2026-02-18 01:58:16 -0300

    Versión estable con login, guardar y borrar objetos

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// ============================
-// Supabase
-// ============================
-const SUPABASE_URL = "https://nfjwpsnztkihqwdbmxja.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_j5n8f34qwwukhOiWRZJqhw_eyFKtJGS";
-
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // ============================
 // Utilidades
