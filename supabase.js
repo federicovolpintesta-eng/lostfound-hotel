dda38bbe278979647ab4bbe472dfb5fea13fe4dc
Federico Volpintesta <federicovolpintesta@gmail.com>
2026-05-13 18:17:30 -0300

    Actualizar resto de archivos (HTML, CSS y config)

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
-const { createClient } = window.supabase;
+  const SUPABASE_URL = "https://nfjwpsnztkihqwdbmxja.supabase.co";
+  const SUPABASE_ANON_KEY = "sb_publishable_j5n8f34qwwukhOiWRZJqhw_eyFKtJGS";
 
-const SUPABASE_URL = "https://nfjwpsnztkihqwdbmxja.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_j5n8f34qwwukhOiWRZJqhw_eyFKtJGS";
-
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+  window.supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY 
+  );
